a0660bfa1a256cdeba6149589553e8e9d2b5b7e5
moniaGh <mnghouili@gmail.com>
2019-06-25 16:59:28 +0100

    add some rules

--- Game.py
+++ Game.py
@@ -1,5 +1,32 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox
+
+
+def checkWinner():
+    if (button1['text'] == 'O' and button2['text'] == 'O' and button3['text'] == 'O' or
+            button4['text'] == 'O' and button5['text'] == 'O' and button6['text'] == 'O' or
+            button7['text'] == 'O' and button8['text'] == 'O' and button9['text'] == 'O' or
+            button1['text'] == 'O' and button4['text'] == 'O' and button7['text'] == 'O' or
+            button2['text'] == 'O' and button5['text'] == 'O' and button8['text'] == 'O' or
+            button3['text'] == 'O' and button6['text'] == 'O' and button9['text'] == 'O'):
+        messagebox._show("Winner of game", "Player 0 is winner")
+        root.quit()
+    elif (button1['text'] == 'X' and button2['text'] == 'X' and button3['text'] == 'X' or
+          button4['text'] == 'X' and button5['text'] == 'X' and button6['text'] == 'X' or
+          button7['text'] == 'X' and button8['text'] == 'X' and button9['text'] == 'X' or
+          button1['text'] == 'X' and button4['text'] == 'X' and button7['text'] == 'X' or
+          button2['text'] == 'X' and button5['text'] == 'X' and button8['text'] == 'X' or
+          button3['text'] == 'X' and button6['text'] == 'X' and button9['text'] == 'X'):
+        messagebox._show("Winner of game o", "Player 1 is winner")
+        root.quit()
+    elif (number ==0):
+        messagebox._show("Match null ", "--")
+        root.quit()
+
+
+
+
 
 root = Tk()
 root.geometry('560x375')
@@ -29,64 +56,122 @@ button8.grid(row=2, column=1, ipadx=50, ipady=50)
 button9 = ttk.Button(root, text=' ', command=lambda: buttonPressed(9))
 button9.grid(row=2, column=2, ipadx=50, ipady=50)
 player = 1
+number = 9
 
 
 def buttonPressed(buttonNumber):
     global player
+    global number
     if buttonNumber == 1 and player == 1:
         button1.config(text='X')
         player = 2
+        number = number - 1
+        button1['state'] = DISABLED
+
     elif buttonNumber == 1 and player == 2:
         button1.config(text="O")
         player = 1
+        number = number - 1
+
+        button1['state'] = DISABLED
+
     elif buttonNumber == 2 and player == 1:
         button2.config(text="X")
         player = 2
+        button2['state'] = DISABLED
+        number = number - 1
+
     elif buttonNumber == 2 and player == 2:
         button2.config(text="O")
         player = 1
+        button2['state'] = DISABLED
+        number = number - 1
+
     elif buttonNumber == 3 and player == 1:
         button3.config(text="X")
         player = 2
+        button3['state'] = DISABLED
+        number = number - 1
+
     elif buttonNumber == 3 and player == 2:
         button3.config(text="O")
         player = 1
+        button3['state'] = DISABLED
+        number = number - 1
+
     elif buttonNumber == 4 and player == 1:
         button4.config(text="X")
         player = 2
+        button4['state'] = DISABLED
+        number = number - 1
+
     elif buttonNumber == 4 and player == 2:
         button4.config(text="O")
         player = 1
+        button4['state'] = DISABLED
+        number = number - 1
+
     elif buttonNumber == 5 and player == 1:
         button5.config(text="X")
         player = 2
+        button5['state'] = DISABLED
+        number = number - 1
+
     elif buttonNumber == 5 and player == 2:
         button5.config(text="O")
         player = 1
+        button5['state'] = DISABLED
+        number = number - 1
+
     elif buttonNumber == 6 and player == 1:
         button6.config(text="X")
         player = 2
+        button6['state'] = DISABLED
+        number = number - 1
+
     elif buttonNumber == 6 and player == 2:
         button6.config(text="O")
         player = 1
+        button6['state'] = DISABLED
+        number = number - 1
+
     elif buttonNumber == 7 and player == 1:
         button7.config(text="X")
         player = 2
+        button7['state'] = DISABLED
+        number = number - 1
+
     elif buttonNumber == 7 and player == 2:
         button7.config(text="O")
         player = 1
+        button7['state'] = DISABLED
+        number = number - 1
+
     elif buttonNumber == 8 and player == 1:
         button8.config(text="X")
         player = 2
+        button8['state'] = DISABLED
+        number = number - 1
+
     elif buttonNumber == 8 and player == 2:
         button8.config(text="O")
         player = 1
+        button8['state'] = DISABLED
+        number = number - 1
+
     elif buttonNumber == 9 and player == 1:
         button9.config(text="X")
         player = 2
+        button9['state'] = DISABLED
+        number = number - 1
+
     elif buttonNumber == 9 and player == 2:
         button9.config(text="O")
         player = 1
+
+        number = number - 1
+        button9['state'] = DISABLED
+    checkWinner()
 
 
 root.mainloop()
